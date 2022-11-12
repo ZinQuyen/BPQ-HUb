0d27c8e22a747a1f9e72c32cf9a16eb9efca44cd
VINH QUYEN <vinhquyen@gmail.com>
2022-11-12 19:38:00 +0700

    update question

--- lib/model/question.dart
+++ lib/model/question.dart
@@ -14,25 +14,25 @@ const List sample_data = [
   {
     "id": 1,
     "question":
-        "Flutter is an open-source UI software development kit created by ______",
+        "Flutter là một bộ công cụ phát triển phần mềm giao diện người dùng mã nguồn mở được tạo bởi ______",
     "options": ['Apple', 'Google', 'Facebook', 'Microsoft'],
     "answer_index": 1,
   },
   {
     "id": 2,
-    "question": "When google release Flutter.",
+    "question": "Khi nào google phát hành Flutter.",
     "options": ['Jun 2017', 'Jun 2017', 'May 2017', 'May 2018'],
     "answer_index": 2,
   },
   {
     "id": 3,
-    "question": "A memory location that holds a single letter or number.",
+    "question": "Vị trí bộ nhớ chứa một chữ cái hoặc số.",
     "options": ['Double', 'Int', 'Char', 'Word'],
     "answer_index": 2,
   },
   {
     "id": 4,
-    "question": "What command do you use to output data to the screen?",
+    "question": "Bạn dùng lệnh gì để xuất dữ liệu ra màn hình?",
     "options": ['Cin', 'Count>>', 'Cout', 'Output>>'],
     "answer_index": 2,
   },
