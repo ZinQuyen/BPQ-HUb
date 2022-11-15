61c0cdbdb8e11a2286caacedb84faba8662c1ed7
VINH QUYEN <vinhquyen@gmail.com>
2022-11-15 19:27:23 +0700

    update screen sound lan 1

--- lib/model/sound.dart
+++ lib/model/sound.dart
@@ -15,6 +15,7 @@ class SoundSetting extends StatefulWidget {
 class _SoundSettingState extends State<SoundSetting> {
   bool val1 = true;
   bool val2 = false;
+  bool val3 = false;
 
   onChangeFunction1(bool newVal1) {
     setState(() {
@@ -25,6 +26,11 @@ class _SoundSettingState extends State<SoundSetting> {
   onChangeFunction2(bool newVal2) {
     setState(() {
       val2 = newVal2;
+    });
+  }
+  onChangeFunction3(bool newVal3) {
+    setState(() {
+      val3 = newVal3;
     });
   }
 
@@ -42,13 +48,30 @@ class _SoundSettingState extends State<SoundSetting> {
             icon: Icon(Icons.arrow_back),
           ),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            customSwitch('Âm nhạc', val1, onChangeFunction1),
-            customSwitch('Âm thanh', val2, onChangeFunction2),
-          ],
+        body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Container(
+            padding: EdgeInsets.only(top: 15),
+            width: double.infinity,
+            height: 500,
+            decoration: BoxDecoration(
+                border: Border.all(
+                  color: Color.fromARGB(255, 101, 217, 246),
+                  width: 5,
+                ),
+                borderRadius: BorderRadius.circular(35)),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                customSwitch('Âm nhạc', val1, onChangeFunction1),
+                customSwitch('Âm thanh', val2, onChangeFunction2),
+                customSwitch('Thông báo', val3, onChangeFunction3),
+
+                Divider(),
+              ],
+            ),
+          ),
         ),
       ),
     );
